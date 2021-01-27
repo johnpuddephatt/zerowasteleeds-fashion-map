9a74f2524f09d68a3818a5c94014c6f15a88237f
John Puddephatt <john@jdp.org.uk>
2021-01-27 19:53:12 +0000

    remove critical transform

--- utils/transforms.js
+++ utils/transforms.js
@@ -1,5 +1,4 @@
 const htmlmin = require('html-minifier')
-const critical = require('critical')
 const buildDir = 'dist'
 
 const shouldTransformHTML = (outputPath) =>
@@ -21,23 +20,4 @@ module.exports = {
         }
         return content
     },
-
-    critical: async function (content, outputPath) {
-        if (shouldTransformHTML(outputPath) && isHomePage(outputPath)) {
-            try {
-                const config = {
-                    base: `${buildDir}/`,
-                    html: content,
-                    inline: true,
-                    width: 1400,
-                    height: 800
-                }
-                const { html } = await critical.generate(config)
-                return html
-            } catch (err) {
-                console.error(err)
-            }
-        }
-        return content
-    }
 }
